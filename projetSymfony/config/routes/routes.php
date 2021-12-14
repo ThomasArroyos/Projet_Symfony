e640fef2752ce2ee0fdaee7ab1e195fe09670c8f
GaetanCharronBalat <37064746+GaetanCharronBalat@users.noreply.github.com>
2021-12-14 10:53:30 +0100

    Routes files

---
+++ projetSymfony/config/routes/routes.php
@@ -0,0 +1,13 @@
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use App\Controller\BlogController;
+
+$routes = new RouteCollection();
+$routes->add('blog_list', new Route('/blog', [
+'_controller' => [BlogController::class, 'list']
+]));
+$routes->add('blog_show', new Route('/blog/{slug}', [
+'_controller' => [BlogController::class, 'show']
+]));
+
+return $routes;
