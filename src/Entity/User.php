9a08541460f2bb7c383aebf0d74931e6a1002e90
Thomas Arroyos <thomas.arroyos@limayrac.fr>
2022-02-13 18:28:10 +0100

    Ajout table User pour page login

---
+++ src/Entity/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Formation", inversedBy="userId")
+     * @ORM\JoinColumn(name="userId", referencedColumnName="id")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $identifiant;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdentifiant(): ?string
+    {
+        return $this->identifiant;
+    }
+
+    public function setIdentifiant(string $identifiant): self
+    {
+        $this->identifiant = $identifiant;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
