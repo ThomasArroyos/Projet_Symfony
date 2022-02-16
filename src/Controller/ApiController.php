158340073c000dd318fd8a8c7639c7fa0e76f7b2
GaetanCharronBalat <gaetan.charronbalat@limayrac.fr>
2022-02-16 16:46:51 +0100

    Ajout d'un évennement depuis le calendrier

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -19,6 +19,61 @@ class ApiController extends AbstractController
     }
 
     /**
+     * @Route ("/api/add", name="api_event_add", methods={"PUT"})
+     */
+    public function addEvent(?Calendar $calendar, Request $request)
+    {
+        //Recupération des données
+        $donnees = json_decode($request->getContent());
+        if(
+            isset($donnees->title) && !empty($donnees->title) &&
+            isset($donnees->start) && !empty($donnees->start) &&
+            isset($donnees->description) && !empty($donnees->description) &&
+            isset($donnees->backgroundColor) && !empty($donnees->backgroundColor) &&
+            isset($donnees->borderColor) && !empty($donnees->borderColor) &&
+            isset($donnees->textColor) && !empty($donnees->textColor)
+        ){
+            //Données complètes
+            //Initialisation du code 200
+            $code = 200;
+
+            //Vérification ID
+            if(!$calendar){
+                //Instantiation rdv
+                $calendar =new Calendar();
+
+                //Changement de code
+                $code = 201;
+            }
+
+            //Hydratation de l'objet
+            $calendar->setTitle($donnees->title);
+            $calendar->setDescription($donnees->description);
+            $calendar->setStart(new \DateTime($donnees->start));
+            if ($donnees->allDay){
+                $calendar->setEnd(new \DateTime($donnees->start));
+            }else{
+                $calendar->setEnd(new \DateTime($donnees->end));
+            }
+            $calendar->setAllDay($donnees->allDay);
+            $calendar->setBackgroundColor($donnees->backgroundColor);
+            $calendar->setBorderColor($donnees->borderColor);
+            $calendar->setTextColor($donnees->textColor);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($calendar);
+            $em->flush();
+
+            //Retourner code
+            return new Response('Ok', $code);
+
+        } else {
+            //Données incomplètes
+            return new Response('Données incomplètes', 404);
+        }
+    }
+
+    /**
      * @Route ("/api/{id}/edit", name="api_event_edit", methods={"PUT"})
      */
     public function majEvent(?Calendar $calendar, Request $request)
