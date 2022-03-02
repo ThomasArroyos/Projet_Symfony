518edd046257ccb64a9b8b4e025a79382080273e
GaetanCharronBalat <gaetan.charronbalat@limayrac.fr>
2022-03-02 21:01:09 +0100

    Modif BDD a tester

---
+++ src/Entity/Specialite.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SpecialiteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SpecialiteRepository::class)
+ */
+class Specialite
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libele;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Matiere::class, inversedBy="specialites")
+     */
+    private $matiere;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Formation::class, mappedBy="specialite")
+     */
+    private $formations;
+
+    public function __construct()
+    {
+        $this->matiere = new ArrayCollection();
+        $this->formations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibele(): ?string
+    {
+        return $this->libele;
+    }
+
+    public function setLibele(string $libele): self
+    {
+        $this->libele = $libele;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Matiere>
+     */
+    public function getMatiere(): Collection
+    {
+        return $this->matiere;
+    }
+
+    public function addMatiere(Matiere $matiere): self
+    {
+        if (!$this->matiere->contains($matiere)) {
+            $this->matiere[] = $matiere;
+        }
+
+        return $this;
+    }
+
+    public function removeMatiere(Matiere $matiere): self
+    {
+        $this->matiere->removeElement($matiere);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Formation>
+     */
+    public function getFormations(): Collection
+    {
+        return $this->formations;
+    }
+
+    public function addFormation(Formation $formation): self
+    {
+        if (!$this->formations->contains($formation)) {
+            $this->formations[] = $formation;
+            $formation->addSpecialite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFormation(Formation $formation): self
+    {
+        if ($this->formations->removeElement($formation)) {
+            $formation->removeSpecialite($this);
+        }
+
+        return $this;
+    }
+}
