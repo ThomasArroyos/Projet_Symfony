eff9cb73285b234c5440fb89155744c9474044a4
GaetanCharronBalat <37064746+GaetanCharronBalat@users.noreply.github.com>
2022-03-03 21:14:30 +0100

    Planning

---
+++ migrations/Version20220303084247.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220303084247 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement ADD temps_fin_recurrence TIME DEFAULT NULL, CHANGE duree_occurence temps_debut_recurrence TIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement ADD duree_occurence TIME DEFAULT NULL, DROP temps_debut_recurrence, DROP temps_fin_recurrence');
+    }
+}
